f33fdbf26999d86cf7293ed7e487381dedb34fef
Cristina Joelle <123904661+CristinaJoelle@users.noreply.github.com>
2023-05-21 21:24:36 -0400

    Added js to generate questions and choices

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,63 @@
+//Variables needed in the getquestion function.
+let currentQuestionArray = 0;
+let choicesEl = document.getElementById("choices-el");
+
+//Timer elements
+let timerEl = document.getElementById("timer-el");
+let time = 60;
+let timerId;
+
+//Fuction to run timer and end quix if the player ran out of time.
+function timeLeft() {
+  time--;
+  timerEl.textContent = time;
+  if (time <= 0) {
+    return endQuiz;
+  }
+}
 // Starts the game when the onclick of startGame is clicked
 function startGame() {
   //Hides the start page
   document.getElementById("homepage").style.display = "none";
-  //Generates the questions
+
+  //Shows the questions page
   document.getElementById("questions").style.display = "block";
 
-  console.log("Game has started!");
+  //Starts timer and shows the time
+  timerId = setInterval(timeLeft, 1000);
+  timerEl.textContent = time;
+  //Calls the questions function to run through the array of questions.
+  getQuestion();
+}
+
+//The function that generates and runs through the questions.
+function getQuestion() {
+  //Gets question from the questions.js and updates the p tag.
+  let currentQuestion = questions[currentQuestionArray];
+  let questionEl = document.getElementById("question-el");
+  questionEl.textContent = currentQuestion.question;
+
+  //Displays the choices from the questions.js in a box.
+  for (var i = 0; i < currentQuestion.choices.length; i++) {
+    let currentChoices = currentQuestion.choices[i];
+    let choicesBtn = document.createElement("button");
+    choicesBtn.setAttribute("class", "currentChoices");
+    choicesBtn.setAttribute("class", currentChoices);
+    choicesBtn.textContent = i + 1 + ". " + currentChoices;
+
+    choicesEl.appendChild(choicesBtn);
+  }
+}
+
+//Function to see if a correct answer was chosen or not.
+function questionAnswer(event) {
+  let selectedChoice = event.target;
+
+  //If wrong answer is selected the player is penalized 5 seconds.
+  if (selectedChoice.value !== questions[currentQuestionArray].answer) {
+    time -= 5;
+  }
+}
+function endQuiz() {
+  console.log("You finished!");
 }
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -2,43 +2,38 @@
 let questions = [
   {
     question: "Which comparison operator is used to show values are NOT equal?",
-    choice1: "==",
-    choice2: ">",
-    choice3: ">=",
-    choice4: "!=",
+    choices: ["==", ">", ">=", "!="],
     answer: 4,
   },
   {
     question: "How would you write 'Hello World' in the console?",
-    choice1: "alert('Hello World')",
-    choice2: "console.log('Hello World')",
-    choice3: "let('Hello World')",
-    choice4: "msBox('Hello World')",
+    choices: [
+      'alert("Hello World")',
+      'console.log("Hello World")',
+      'let("Hello World")',
+      'msBox("Hello World")',
+    ],
     answer: 2,
   },
   {
     question:
       "What element would you use to link the JavaScript file to the HTML file?",
-    choice1: "<link>",
-    choice2: "<tag>",
-    choice3: "<script>",
-    choice4: "<a>",
+    choices: ["<link>", "<tag>", "<script>", "<a>"],
     answer: 3,
   },
   {
     question: "Which of the following is a boolean value?",
-    choice1: "true",
-    choice2: "1997",
-    choice3: "'Hello'",
-    choice4: "null",
+    choices: ["true", "1997", '"Hello"', "null"],
     answer: 1,
   },
   {
     question: "How would you find an element by it's ID?",
-    choice1: "getelementsByClassName",
-    choice2: "querySelectorAll",
-    choice3: "getElementById",
-    choice4: "getElementsByTagName",
+    choices: [
+      "getelementsByClassName",
+      "querySelectorAll",
+      "getElementById",
+      "getElementsByTagName",
+    ],
     answer: 3,
   },
 ];
